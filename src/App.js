c2bbcf6f1ef337deed449c7706d6f6af5878e338
VithleshAg <vithlesh14@gmail.com>
2020-02-15 15:49:11 +0530

    third

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const Body = () => (
-  <p className="App-intro">
-    To get started,edit <code>src/App.js</code> and save to reload
-  </p>
+const Body = props => (
+  <div>
+    <p className="App-intro">{props.text}</p>
+    <p className="App-intro">{props.text2}</p>
+    <p className="App-intro">{props.myFunction(567, 78)}</p>
+  </div>
 );
 
 // const Body = () => {
@@ -29,18 +31,43 @@ class Header extends Component {
     return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
+        <h1 className="App-title">{this.props.title}</h1>
+        <div>{this.props.num}</div>
+        <div>{this.props.myArr[2]}</div>
+        <div>{this.props.myObj.a}</div>
+        <div>{this.props.myObj.b}</div>
+        <div>{JSON.stringify(this.props.myObj)}</div>
+        <div>{this.props.myFunction(100, 1299)}</div>
       </header>
     );
   }
 }
 
 class App extends Component {
+  add = (a, b) => {
+    return a + b;
+  };
   render() {
     return (
       <div className="App">
-        <Header />
-        <Body />
+        <Header
+          title="heyyyy"
+          num={56}
+          myArr={[1, 2, 3]}
+          myObj={{ a: 1, b: 10 }}
+          // myFunction={(a, b) => a + b}
+          myFunction={this.add}
+        />
+        <Body
+          text="I am Alien"
+          text2="I am alien tooooooo"
+          myFunction={this.add}
+        />
+        <Body
+          text="I am Alien"
+          text2="I am alien tooooooo"
+          myFunction={this.add}
+        />
       </div>
     );
   }
