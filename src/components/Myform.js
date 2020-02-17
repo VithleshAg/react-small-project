66f575a6a51f9b2768ff29a8f53a3892ee35fd68
VithleshAg <vithlesh14@gmail.com>
2020-02-17 22:59:26 +0530

    tenth

--- src/components/Myform.js
+++ src/components/Myform.js
@@ -3,47 +3,59 @@ import React from "react";
 export default class Myform extends React.Component {
   state = {
     name: "",
+    favName: "",
     rememberMe: false,
     selectMe: "Mrs."
   };
 
   handleChange = event => {
-    this.setState({ name: event.target.value });
+    const isCheckbox = event.target.type === "checkbox";
+    this.setState({
+      [event.target.name]: isCheckbox
+        ? event.target.checked
+        : event.target.value
+    });
   };
 
-  handleCheck = event => {
-    this.setState({ rememberMe: event.target.checked });
-  };
-
-  handleSelect = event => {
-    this.setState({ selectMe: event.target.value });
-  };
-
-  handleSubmit = () => {
+  handleSubmit = event => {
+    event.preventDefault();
     console.log(this.state);
   };
 
   render() {
     return (
-      <div>
-        <input onChange={this.handleChange} />
-        <br />
-        <textarea value={this.state.name} />
+      <form onSubmit={this.handleSubmit}>
         <input
+          name="name"
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
+        <br />
+        <input
+          name="favName"
+          value={this.state.favName}
+          onChange={this.handleChange}
+        />
+        <input
+          name="rememberMe"
           type="checkbox"
           checked={this.state.rememberMe}
-          onChange={this.handleCheck}
+          onChange={this.handleChange}
         />
         <div>
-          <select value={this.state.selectMe} onChange={this.handleSelect}>
+          <select
+            name="selectMe"
+            value={this.state.selectMe}
+            onChange={this.handleChange}
+          >
             <option>Mr.</option>
             <option>Mrs.</option>
             <option>Ms.</option>
             <option>Mss.</option>
           </select>
         </div>
-        <button onClick={this.handleSubmit}>Submit</button>
-      </div>
+        <button type="submit">Submit</button>
+      </form>
     );
   }
 }
