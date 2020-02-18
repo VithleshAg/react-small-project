1c70afa85febe3b8bbc4f9450fc3b2349d3b78c9
VithleshAg <vithlesh14@gmail.com>
2020-02-18 14:43:08 +0530

    13

--- src/components/fetchRandomUser.js
+++ src/components/fetchRandomUser.js
@@ -3,29 +3,45 @@ import React from "react";
 export default class FetchRandomUser extends React.Component {
   state = {
     loading: true,
-    person: null
+    people: []
   };
   async componentDidMount() {
-    const url = "https://api.randomuser.me/";
+    const url = "https://api.randomuser.me/?results=5";
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({ person: data.results[0], loading: false });
+    this.setState({ people: data.results, loading: false });
   }
   render() {
     if (this.state.loading) {
       return <div>Loading...</div>;
     }
-    if (!this.state.person) {
+    if (!this.state.people.length) {
       return <div>Didn't get person details </div>;
     }
+
+    // const peopleJSX = [];
+
+    // this.state.people.forEach(person => {
+    //   peopleJSX.push(
+    //     <div key={person.login.uuid}>
+    //       <div>{person.name.title}</div>
+    //       <div>{person.name.first}</div>
+    //       <div>{person.name.last}</div>
+    //       <img src={person.picture.large} />
+    //     </div>
+    //   );
+    // });
+
     return (
       <div>
-        <div>{this.state.person.name.title}</div>
-        <div>{this.state.person.name.first}</div>
-        <div>{this.state.person.name.last}</div>
-        <div>
-          <img src={this.state.person.picture.large} />
-        </div>
+        {this.state.people.map(person => (
+          <div key={person.login.uuid}>
+            <div>{person.name.title}</div>
+            <div>{person.name.first}</div>
+            <div>{person.name.last}</div>
+            <img src={person.picture.large} />
+          </div>
+        ))}
       </div>
     );
   }
